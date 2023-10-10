fe83a791ef8625175d500313aa2f5ca70a25ab39
anhtudo97 <anhtudo97@gmail.com>
2023-10-11 03:18:32 +0700

    feat: api key

---
+++ src/services/apiKey.service.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { randomBytes } = require('crypto');
+const apiKeyModel = require('../models/apiKey.model');
+
+const findById = async (key) => {
+//   const newKey = apiKeyModel.create({
+//     key: randomBytes(64).toString('hex'),
+//     permissions: ['0000'],
+//   });
+//   console.log(newKey);
+  const objKey = await apiKeyModel.findOne({ key, status: true }).lean();
+  return objKey;
+};
+
+module.exports = {
+  findById,
+};
