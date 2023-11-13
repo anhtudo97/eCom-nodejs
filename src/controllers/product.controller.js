e95bba9b9da2ee2ee1eba51dfa44f22ea17d5188
tuda.nami <tuda@nami.trade>
2023-11-13 15:46:42 +0700

    feat: controller + services

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { CREATED, SuccessResponse } = require('../core/success.response');
+const AccessService = require('../services/access.service');
+
+class AccessController {
+  handleRefreshToken = async (req, res, next) => {
+    new SuccessResponse({
+      message: 'Get token success!',
+      metadata: await AccessService.handleRefreshToken(req.body.refreshToken),
+    }).send(res);
+  };
+
+  login = async (req, res, next) => {
+    new SuccessResponse({
+      metadata: await AccessService.login(req.body),
+    }).send(res);
+  };
+
+  logout = async (req, res, next) => {
+    new SuccessResponse({
+      message: 'Logout success!',
+      metadata: await AccessService.logout(req.keyStore),
+    }).send(res);
+  };
+
+  signUp = async (req, res, next) => {
+    console.log(`[P]::signUp::`, req.body);
+
+    new CREATED({
+      mesage: 'Registered OK!',
+      metadata: await AccessService.signUp(req.body),
+      options: {
+        limit: 10,
+      },
+    }).send(res);
+
+    // return res.status(201).json(await AccessService.signUp(req.body));
+  };
+}
+
+module.exports = new AccessController();
