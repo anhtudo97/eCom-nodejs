8de44731e6022d3482c0a0e1fdccf14f17daebc5
Alex Dou <anhtudo97@gmail.com>
2025-08-14 01:01:26 +0700

    feat: update for check

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,23 +4,29 @@ const express = require('express');
 const { apiKey, permission } = require('../auth/checkAuth');
 const router = express.Router();
 
+router.get('/checkstatus', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api oke'
+    })
+})
+
 // CHECK API KEY
+// router.use(apiKey);
+// // CHECK API KEY
 
-router.use(apiKey);
-// CHECK API KEY
+// router.use(permission('0000'));
 
-router.use(permission('0000'));
-
-// ROUTES
-router.use('/v1/api/discount', require('./discount'));
-router.use('/v1/api/cart', require('./cart'));
-router.use('/v1/api/checkout', require('./checkout'));
-router.use('/v1/api/inventory', require('./inventory'));
-router.use('/v1/api/product', require('./product'));
-router.use('/v1/api/upload', require('./upload'));
-router.use('/v1/api/comment', require('./comment'));
-router.use('/v1/api/notification', require('./notification'));
-router.use('/v1/api', require('./access'));
+// // ROUTES
+// router.use('/v1/api/discount', require('./discount'));
+// router.use('/v1/api/cart', require('./cart'));
+// router.use('/v1/api/checkout', require('./checkout'));
+// router.use('/v1/api/inventory', require('./inventory'));
+// router.use('/v1/api/product', require('./product'));
+// router.use('/v1/api/upload', require('./upload'));
+// router.use('/v1/api/comment', require('./comment'));
+// router.use('/v1/api/notification', require('./notification'));
+// router.use('/v1/api', require('./access'));
 // router.get('/', (req, res, next) => {
 //   // const strCompress = 'Hello Alex';
 //   return res.status(200).json({
